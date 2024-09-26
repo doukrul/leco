fb20179258885f56b1eea2ca1bb99ddc51367a86
doukrul <1079839946@qq.com>
2024-09-26 15:35:27 +0800

    new

--- Csharp/NeuralNetWork/leco/P380/test.cs
+++ Csharp/NeuralNetWork/leco/P380/test.cs
@@ -3,43 +3,70 @@ using System.Collections.Generic;
 /// <summary>
 /// Dictionary:c#中的一种泛型集合,用来存储键值对(hash)
 /// </summary>
-public class RandomizedSet {
+public class RandomizedSet
+{
     private Random random;
     private List<int> list;
-    private Dictionary<int,int> indexMap;
+    private Dictionary<int, int> indexMap;
     //初始化
-    public RandomizedSet() {
-        this.random=new Random();
-        list=new List<int>();
+    public RandomizedSet()
+    {
+        this.random = new Random();
+        list = new List<int>();
         //indexMap[key,value]键值对
-        indexMap=new Dictionary<int, int>();
-        
+        indexMap = new Dictionary<int, int>();
+
     }
-    
-    public bool Insert(int val) {
-        if(indexMap.ContainsKey(val)){
+
+    public bool Insert(int val)
+    {
+        if (indexMap.ContainsKey(val))
+        {
             return false;
         }
-        else{
+        else
+        {
             list.Add(val);
+            //看来是val,但indexMap[key]实际为值,记录元素val在list中的索引
+            //list.Add添加元素到末尾,所以是-1即为索引
+            indexMap[val] = list.Count - 1;
         }
-        //看来是val,但indexMap[key]实际为值,记录元素val在list中的索引
-        //list.Add添加元素到末尾,所以是-1即为索引
-        indexMap[val]=list.Count-1;
+
+
         return true;
     }
-    
-    public bool Remove(int val) {
-        if(!indexMap.ContainsKey(val)){
+
+    public bool Remove(int val)
+    {
+        if (!indexMap.ContainsKey(val))
+        {
             // list.Remove(val);这样写时间复杂度为O(n)而不是O(1)
             return false;
         }
-        
+        else
+        {
+            //存在该元素,要删除
+            //最后一位元素
+            int lastElement = list[list.Count - 1];
+            //要删除的元素索引
+            int indexToRemove = indexMap[val];
+            //最后一位元素挪到要删除的位置
+            list[indexToRemove] = lastElement;
+            //更新字典的最后一位元素的索引
+            indexMap[lastElement] = indexToRemove;
+            //删除最后一位
+            list.RemoveAt(list.Count - 1);
+            //删除indexMap中的val这个值
+            indexMap.Remove(val);
+        }
         return true;
     }
-    
-    public int GetRandom() {
-        return 0;
+
+    public int GetRandom()
+    {
+        int randomIndex = random.Next(1, list.Count);
+
+        return list[randomIndex];
     }
 }
 
