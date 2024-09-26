c518e4b93a93c39dfc5172d76a9faef2abfb2fbf
doukrul <1079839946@qq.com>
2024-09-26 16:37:34 +0800

    new

---
+++ Csharp/NeuralNetWork/leco/P134/test.cs
@@ -0,0 +1,9 @@
+public class Solution134 {
+    public int CanCompleteCircuit(int[] gas, int[] cost) {
+        int currentoil=0;
+        for(int i=0;i<gas.Length;++i){
+            
+        }
+        return -1;
+    }
+}
--- Csharp/NeuralNetWork/leco/P238/test.cs
+++ Csharp/NeuralNetWork/leco/P238/test.cs
@@ -1,16 +1,20 @@
-public class Solution {
-    public int[] ProductExceptSelf(int[] nums) {
-        int []answer=new int[nums.Length];
-        int n=nums.Length;
-        int pre=1;
-        int last=1;
-        for(int i=0;i<n;i++){
-            answer[i]=pre;
-            pre*=nums[i];
+public class Solution
+{
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        int[] answer = new int[nums.Length];
+        int n = nums.Length;
+        int pre = 1;
+        int last = 1;
+        for (int i = 0; i < n; ++i)
+        {
+            answer[i] = pre;
+            pre *= nums[i];
         }
-        for(int i=0;i<n;i++){
-            answer[i]*=last;
-            last*=nums[i];
+        for (int i = n - 1; i >= 0; --i)
+        {
+            answer[i] *= last;
+            last *= nums[i];
         }
         return answer;
     }
