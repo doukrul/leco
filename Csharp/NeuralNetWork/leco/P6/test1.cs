70e10131aced7831a3161e5b383f801dd4228c9d
doukrul <1079839946@qq.com>
2024-09-30 10:19:52 +0800

    new

--- Csharp/NeuralNetWork/leco/P6/test1.cs
+++ Csharp/NeuralNetWork/leco/P6/test1.cs
@@ -1,5 +1,9 @@
+using System.Text;
+
 public class Solution6 {
     public string Convert(string s, int numRows) {
+        StringBuilder sb=new StringBuilder();
         
-    }
+        return sb.ToString();    
+        }
 }
