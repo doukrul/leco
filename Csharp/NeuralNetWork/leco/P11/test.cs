ca4108df78e99210af016691ac206556e806a5f2
doukrul <1079839946@qq.com>
2024-10-14 21:38:44 +0800

    new

---
+++ Csharp/NeuralNetWork/leco/P11/test.cs
@@ -0,0 +1,7 @@
+public class SolutionP11test1 {
+    public int MaxArea(int[] height) {
+        int prev=0,last=height.Length;
+        
+        return 0;
+    }
+}
--- Csharp/NeuralNetWork/leco/P167/test1.cs
+++ Csharp/NeuralNetWork/leco/P167/test1.cs
@@ -1,10 +1,26 @@
-public class SolutionP167test1 {
-    public int[] TwoSum(int[] numbers, int target) {
-        int i1=0,i2=0;
-        while(i1<numbers.Length&&i2<numbers.Length){
-            
+public class SolutionP167test1
+{
+
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        int i1 = 0, i2 = numbers.Length - 1;
+        while (i1 < i2)
+        {
+            int sum = numbers[i1] + numbers[i2];
+            if (sum == target)
+            {
+                return new int[2] { i1 + 1, i2 + 1 };
+            }
+            if (sum > target)
+            {
+                --i2;
+            }
+            else
+            {
+                ++i1;
+            }
         }
-        return new int[2]{i1,i2};
+        return new int[2] { 0, 0 };
 
     }
 }
