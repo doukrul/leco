89879f4546a146bb8c75ab9210206e82bedca5dd
doukrul <1079839946@qq.com>
2024-10-14 22:29:25 +0800

    new

--- Csharp/NeuralNetWork/leco/P11/test.cs
+++ Csharp/NeuralNetWork/leco/P11/test.cs
@@ -1,7 +1,29 @@
-public class SolutionP11test1 {
-    public int MaxArea(int[] height) {
-        int prev=0,last=height.Length;
-        
-        return 0;
+public class SolutionP11test1
+{
+    public int MaxArea(int[] height)
+    {
+        int prev = 0, last = height.Length - 1;
+
+        int prevh_max = height[0];
+        int lasth_max = height[height.Length - 1];
+        int area = 0;
+        while (last > prev)
+        {
+            int width = last - prev;
+            int newarea = width * Math.Min(prevh_max, lasth_max);
+            area = Math.Max(area, newarea);
+            if (prevh_max > lasth_max)
+            {
+                last--;
+                lasth_max = Math.Max(height[last], lasth_max);
+            }
+            else
+            {
+                prev++;
+                prevh_max = Math.Max(height[prev], prevh_max);
+            }
+
+        }
+        return area;
     }
 }
