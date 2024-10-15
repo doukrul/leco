bf68c1c21e4851e30283b5df079e0fc4b8836c25
doukrul <1079839946@qq.com>
2024-10-15 22:42:30 +0800

    new

--- Csharp/NeuralNetWork/leco/P3/test1.cs
+++ Csharp/NeuralNetWork/leco/P3/test1.cs
@@ -1,5 +1,43 @@
+using System.Text;
 public class SolutionP3test1 {
-    public int LengthOfLongestSubstring(string s) {
-        
+    public int LengthOfLongestSubstring(string s)
+    {
+        //滑动窗口
+        int result = 0;
+        int sublength = 0;
+        int subi=0;
+
+        //子串
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (!Haschar(s[i], sb.ToString()))
+            {
+                sb.Append(s[i]);
+                
+            }
+            else
+            {
+                result = Math.Max(result, sb.Length);i=i-sb.Length;
+                sb.Clear();
+                
+            }
+        }
+        result = Math.Max(result, sb.Length);
+        return result;
+    }
+    //重复返回true,否则返回false
+    public bool Haschar(char c, string s)
+    {
+        HashSet<char> chars = new HashSet<char>() { c };
+
+        foreach (char cin in s)
+        {
+            if (chars.Contains(cin))
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
