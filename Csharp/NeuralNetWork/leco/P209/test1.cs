09fdf880492e2ca4bdd76e05d93c1c801dd17b06
doukrul <1079839946@qq.com>
2024-10-15 18:38:51 +0800

    new

--- Csharp/NeuralNetWork/leco/P209/test1.cs
+++ Csharp/NeuralNetWork/leco/P209/test1.cs
@@ -1,26 +1,25 @@
 public class SolutionP209test1
 {
+    //双层循环时间复杂度O(n)
     public int MinSubArrayLen(int target, int[] nums)
     {
-        int result = nums.Length - 1;
-        int sum = 0;
-        foreach (int n in nums)
-        {
-            sum += n;
-        }
-        if (sum < target)
-        {
-            return 0;
-        }
-        Array.Sort(nums);
+        int result = int.MaxValue;
+        int sublength = 0;
+
         for (int i = 0; i < nums.Length; i++)
         {
-            if(sum>target){
-                sum-=nums[i];
-                result--;
+            int sum = 0;
+            for (int j = i; j < nums.Length; j++)
+            {
+                sum += nums[j];
+                if (sum >= target)
+                {
+                    sublength = j - i + 1;
+                    result = Math.Min(result, sublength);
+                    break;
+                }
             }
-
         }
-        return result;
+        return result==int.MaxValue?0:result;
     }
 }
