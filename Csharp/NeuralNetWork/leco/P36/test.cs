99e074bd5cc65eede068222725e0965d8f62ecc8
doukrul <1079839946@qq.com>
2024-10-19 17:10:33 +0800

    new

--- Csharp/NeuralNetWork/leco/P36/test.cs
+++ Csharp/NeuralNetWork/leco/P36/test.cs
@@ -1,6 +1,54 @@
-public class SolutionP36test1 {
-    public bool IsValidSudoku(char[][] board) {
+public class SolutionP36test1
+{
+    public bool IsValidSudoku(char[][] board)
+    {
+
+        HashSet<char> rowcheck = new HashSet<char>();
+        HashSet<char> colcheck = new HashSet<char>();
+        HashSet<char> boxcheck = new HashSet<char>();
         
+
+        //判断行,判断列
+        for (int i = 0; i < board.Length; i++)
+        {   
+            bool[]row = new bool[board.Length];
+            bool[]col = new bool[board.Length];
+            bool[]box = new bool[board.Length];
+            for (int j = 0; j < board[i].Length; j++)
+            {
+                if(board[i][j]!='.'){
+                    int num=board[i][j]-'1';
+                    if(row[num]){
+                        return false;
+                    }
+                    row[num]=true;
+                }
+                
+                if(board[j][i]!='.'){
+                    int num=board[j][i]-'1';
+                    if(row[num]){
+                        return false;
+                    }
+                    row[num]=true;
+                }
+                
+                //判断3*3
+                //判断在哪一块
+                int x = i / 3, y = j / 3;
+                for (int tempx = x * 3; tempx < x * 3 + 3; tempx++)
+                {
+                    for (int tempy = y * 3; tempy < y * 3 + 3; tempy++)
+                    {
+                        if (tempx != i && tempy != j)
+                        {
+                            if(board[tempx][tempy]==board[i][j]){
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
         return true;
     }
 }
