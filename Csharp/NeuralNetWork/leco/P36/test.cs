41dae02e59e0d3eeb3632c4d25972e54be0ed6be
doukrul <1079839946@qq.com>
2024-10-20 17:38:31 +0800

    new

--- Csharp/NeuralNetWork/leco/P36/test.cs
+++ Csharp/NeuralNetWork/leco/P36/test.cs
@@ -2,49 +2,54 @@ public class SolutionP36test1
 {
     public bool IsValidSudoku(char[][] board)
     {
-
-        HashSet<char> rowcheck = new HashSet<char>();
-        HashSet<char> colcheck = new HashSet<char>();
-        HashSet<char> boxcheck = new HashSet<char>();
-        
-
-        //判断行,判断列
         for (int i = 0; i < board.Length; i++)
-        {   
-            bool[]row = new bool[board.Length];
-            bool[]col = new bool[board.Length];
-            bool[]box = new bool[board.Length];
+        {
+            bool[] row = new bool[board.Length];
+            bool[] col = new bool[board.Length];
+
             for (int j = 0; j < board[i].Length; j++)
             {
-                if(board[i][j]!='.'){
-                    int num=board[i][j]-'1';
-                    if(row[num]){
-                        return false;
-                    }
-                    row[num]=true;
-                }
-                
-                if(board[j][i]!='.'){
-                    int num=board[j][i]-'1';
-                    if(row[num]){
-                        return false;
-                    }
-                    row[num]=true;
-                }
-                
-                //判断3*3
-                //判断在哪一块
-                int x = i / 3, y = j / 3;
-                for (int tempx = x * 3; tempx < x * 3 + 3; tempx++)
+                //判断行,判断列
+                if (board[i][j] != '.')
                 {
-                    for (int tempy = y * 3; tempy < y * 3 + 3; tempy++)
+                    int num = board[i][j] - '1';
+                    if (row[num])
                     {
-                        if (tempx != i && tempy != j)
+                        return false;
+                    }
+                    row[num] = true;
+                }
+
+                if (board[j][i] != '.')
+                {
+                    int num = board[j][i] - '1';
+                    if (col[num])
+                    {
+                        return false;
+                    }
+                    col[num] = true;
+                }
+            }
+        }
+
+        //判断3*3
+        //外层9*9划分成9块3*3
+        for (int sign = 0; sign < 9; sign++)
+        {
+            bool[] box = new bool[board.Length];
+            //每块3*3内部
+            for (int i = sign % 3 * 3; i < sign % 3 * 3 + 3; i++)
+            {
+                for (int j = sign / 3 * 3; j < sign / 3 * 3 + 3; j++)
+                {
+                    if (board[i][j] != '.')
+                    {
+                        int num = board[i][j] - '1';
+                        if (box[num])
                         {
-                            if(board[tempx][tempy]==board[i][j]){
-                                return false;
-                            }
+                            return false;
                         }
+                        box[num] = true;
                     }
                 }
             }
