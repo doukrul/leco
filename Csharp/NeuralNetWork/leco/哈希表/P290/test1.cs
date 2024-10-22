f79e834832c7abdad8fb05b2a22e251e07fb14a2
doukrul <1079839946@qq.com>
2024-10-22 20:37:14 +0800

    new

--- Csharp/NeuralNetWork/leco/哈希表/P290/test1.cs
+++ Csharp/NeuralNetWork/leco/哈希表/P290/test1.cs
@@ -1,6 +1,41 @@
-public class SolutionP290test1 {
-    public bool WordPattern(string pattern, string s) {
-        
+using System.Text;
+
+public class SolutionP290test1
+{
+    //单个字典,失败了通过不了
+    public bool WordPattern(string pattern, string s)
+    {
+        Dictionary<char, string> words = new Dictionary<char, string>();
+
+        StringBuilder word = new StringBuilder();
+        int sign = 0;
+        for (int j = 0; j <= s.Length; j++)
+        {
+            if (s[j] != ' '&&j<s.Length)
+            {
+                word.Append(s[j]);
+            }
+            else
+            {
+                if (words.ContainsKey(pattern[sign]))
+                {
+                    if (!words[pattern[sign]].Equals(word.ToString()))
+                    {
+                        return false;
+                    }
+                    sign++;
+                }
+                else
+                {
+                    words.Add(pattern[sign], word.ToString());
+                    sign++;
+                    word.Clear();
+                }
+
+            }
+        }
+
+
         return true;
     }
 }
---
+++ Csharp/NeuralNetWork/leco/哈希表/P290/test2.cs
@@ -0,0 +1,47 @@
+using System.Text;
+
+public class Solution
+{
+    public bool WordPattern(string pattern, string s)
+    {
+        //成功
+        Dictionary<char, string> ptowords = new Dictionary<char, string>();
+
+        Dictionary<string,char> wordtop=new Dictionary<string,char>();
+        int sign=0;
+        int start = 0;
+        for (int i = 0; i <= s.Length; i++)
+        {
+            if(i==s.Length||s[i]==' '){
+                //获得单词
+                string word=s.Substring(start,i-start);
+                start=i+1;
+                
+                if(sign==pattern.Length){
+                    return false;
+                }
+                if(ptowords.ContainsKey(pattern[sign])){
+                    if(!ptowords[pattern[sign]].Equals(word)){
+                        return false;
+                    }
+                }else{
+                    ptowords.Add(pattern[sign],word);
+                }
+                if(wordtop.ContainsKey(word)){
+                    if(wordtop[word]!=pattern[sign]){
+                        return false;
+                    }
+                }else{
+                    wordtop.Add(word,pattern[sign]);
+                }
+                sign++;
+
+            }
+        }
+        if(ptowords.Count!=wordtop.Count||sign!=pattern.Length){
+            return false;
+        }
+
+        return true;
+    }
+}
